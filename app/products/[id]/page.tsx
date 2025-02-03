2aaaee19114007045e51069fc982b994317d67bd
padmajaar <padmajaa.r@gmail.com>
2025-02-03 14:57:41 -0600

    Added CSS

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,18 +1,29 @@
 import NotFoundPage from "@/app/not-found";
 import { products } from "@/app/product-data";
+import Image from "next/image";
 
 export default function ProductDetailsPage({ params }: { params: { id: string } }) {
     const product = products.find(item => item.id === params.id);
 
-    if(!product){
+    if (!product) {
         return <NotFoundPage />;
     }
     return (
-        <>
-            <h1>{product!.name}</h1>
-            <p>{product!.price}</p>
-            <h3>Description</h3>
-            <p>{product!.description}</p>
-        </>
-    )
+        <div className="container mx-auto p-8 flex flex-col md:flex-row">
+            <div className="md:w-1/2 mb-4 md:mb-0 md:mr-8">
+                <Image
+                    src={'/' + product.imageUrl}
+                    alt={product.name}
+                    width={500}
+                    height={300}
+                    className="w-full h-auto rounded-lg shadow-md" />
+            </div>
+            <div className="md:w-1/2">
+                <h1 className="text-4xl font-bold mb-4">{product.name}</h1>
+                <p className="text-2xl text-gray-600 mb-6">${product.price}</p>
+                <h3 className="text-2xl font-semibold mb-2">Description</h3>
+                <p className="text-gray-700">{product.description}</p>
+            </div>
+        </div>
+    );
 }
