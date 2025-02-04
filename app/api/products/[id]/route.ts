a677561d92e825e9ec18d2c2d762f5c2abb7ba97
padmajaar <padmajaa.r@gmail.com>
2025-02-04 14:17:39 -0600

    Creating GET route handler for product detail page

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,28 @@
+import { products } from "@/app/product-data";
+import { NextRequest } from "next/server";
+
+type Params = {
+  id: string;
+};
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Params }
+) {
+  const productId = params.id;
+
+  const product = products.find((p) => p.id === productId);
+
+  if(!product){
+    return new Response('Product not found', {
+        status: 404
+    });
+  }
+
+  return new Response(JSON.stringify(product), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
