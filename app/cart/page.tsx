7bdaf94427f4590bba49c315ed7d00d272190a27
padmajaar <padmajaa.r@gmail.com>
2025-02-05 16:03:33 -0600

    added CSS for cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -10,22 +10,25 @@ export default function CartPage() {
     const cartProducts = cartIds.map(id => products.find(p => p.id === id));
 
     return (
-        <>
-            <h1>Shopping Cart</h1>
-            {cartProducts?.length > 0 ? (
-                cartProducts.map(product => {
-                    if (!product) return null;
-                    return (
-                        <Link key={product.id} href={"/products/" + product.id}>
-                            <h2>{product.name}</h2>
-                            <p>${product.price}</p>
-                        </Link>
-
-                    )
-                })
-            ) : (
-                <p>Your cart is empty.</p>
-            )}
-        </>
+        <div className="container mx-auto p-8">
+            <h1 className="text-4xl font-bold mb-8">Shopping Cart</h1>
+            <ul className="space-y-4">
+                {cartProducts?.length > 0 ? (
+                    cartProducts.map(product => {
+                        if (!product) return null;
+                        return (
+                            <li key={product.id} className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300">
+                                <Link key={product.id} href={"/products/" + product.id}>
+                                    <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+                                    <p className="text-gray-600">${product.price}</p>
+                                </Link>
+                            </li>
+                        )
+                    })
+                ) : (
+                    <p>Your cart is empty.</p>
+                )}
+            </ul>
+        </div>
     )
 }
