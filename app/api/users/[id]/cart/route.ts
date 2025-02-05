ba6189246a0c9946205c73688e8548e311a063f1
padmajaar <padmajaa.r@gmail.com>
2025-02-05 14:00:04 -0600

    Creating DELETE remote to cart endpoint

--- app/api/users/[id]/cart/route.ts
+++ app/api/users/[id]/cart/route.ts
@@ -61,3 +61,24 @@ export async function POST(
     },
   });
 }
+
+
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: Params }
+  ) {
+    const userId = params.id;
+    const body: CartBody = await request.json();
+    const productId = body.productId;
+  
+    carts[userId] = carts[userId] ? carts[userId].filter(pid => pid !== productId) : [];
+    const cartProducts = carts[userId].map((id) =>
+        products.find((p) => p.id === id)
+      );
+    return new Response(JSON.stringify(cartProducts), {
+      status: 202,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
