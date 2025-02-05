25993de2ac886508884ddf0b69ae772f7a20d4cd
padmajaar <padmajaa.r@gmail.com>
2025-02-05 13:53:47 -0600

    Creating POSt add to cart endpoint

--- app/api/users/[id]/cart/route.ts
+++ app/api/users/[id]/cart/route.ts
@@ -5,11 +5,14 @@ type ShoppingCart = Record<string, string[]>;
 type Params = {
   id: string;
 };
+type CartBody = {
+  productId: string;
+};
 
 const carts: ShoppingCart = {
   "1": ["123", "345"],
   "2": ["345", "456"],
-  "3": ["234"],
+  "3": ["234"], 
 };
 
 export async function GET(
@@ -33,6 +36,26 @@ export async function GET(
 
   return new Response(JSON.stringify(cartProducts), {
     status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: Params }
+) {
+  const userId = params.id;
+  const body: CartBody = await request.json();
+  const productId = body.productId;
+
+  carts[userId] = carts[userId] ? carts[userId].concat(productId) : [productId];
+  const cartProducts = carts[userId].map((id) =>
+    products.find((p) => p.id === id)
+  );
+  return new Response(JSON.stringify(cartProducts), {
+    status: 201,
     headers: {
       "Content-Type": "application/json",
     },
