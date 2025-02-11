444d874650a9ec3a4308d47469f621c07e645392
padmajaar <padmajaa.r@gmail.com>
2025-02-11 10:25:44 -0600

    Implemented Addtocart and removefromcar functionality

--- app/ProductsList.tsx
+++ app/ProductsList.tsx
@@ -4,8 +4,30 @@ import Image from 'next/image';
 import Link from 'next/Link';
 
 import { Product } from "./product-data";
+import { useState } from 'react';
 
-export default function ProductsList({ products }: { products: Product[] }) {
+export default function ProductsList({ products, initialCartProducts }: { products: Product[], initialCartProducts: Product[] }) {
+
+    const [cartProducts, setCartProducts] = useState(initialCartProducts);
+
+    async function addToCart(productId: string) {
+        const response = await fetch('http://localhost:3000/api/users/2/cart', {
+            method: 'POST',
+            body: JSON.stringify({
+                productId,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+
+        const updatedCartProducts = await response.json();
+        setCartProducts(updatedCartProducts);
+    }
+
+    function productIsInCart(productId: string) {
+        return cartProducts.some(cp => cp.id === productId);
+    }
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
             {products.map(product => (
@@ -24,7 +46,21 @@ export default function ProductsList({ products }: { products: Product[] }) {
                     </div>
 
                     <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
-                    <p  className="text-gray-600">${product.price}</p>
+                    <p className="text-gray-600">${product.price}</p>
+                    {
+                        productIsInCart(product.id) ? (
+                            <button onClick={(e) => {
+                                e.preventDefault();
+                                console.log('Removing from cart')
+                            }}>Remove from Cart</button>
+                        ) : (
+                            <button onClick={(e) => {
+                                e.preventDefault();
+                                addToCart(product.id)
+                            }}>Add to Cart</button>
+                        )
+                    }
+
                 </Link>
             ))}
         </div>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,13 +1,17 @@
 import ProductsList from "../ProductsList";
 
 export default async function ProductsPage() {
-    const response = await fetch('http://localhost:3000/api/products')
+    const response = await fetch('http://localhost:3000/api/products');
     const products = await response.json();
+    const cartResponse = await fetch('http://localhost:3000/api/users/2/cart', {
+        cache: 'no-cache',
+    });
+    const cartProducts = await cartResponse.json();
 
     return (
         <div className="container mx-auto p-8">
             <h1 className="text-4xl font-bold mb-8">Products</h1>
-            <ProductsList products={products} />
+            <ProductsList products={products} initialCartProducts={cartProducts} />
         </div>
     )
 }
